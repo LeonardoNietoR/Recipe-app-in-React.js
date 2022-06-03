ab1a1f754f0c4e10e66ede1cb59f570927eefeca
LeonardoNietoR <leonardo.nieto.r@gmail.com>
2022-06-02 23:28:15 -0500

    3 update main header

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,13 +1,23 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import MainHeader from "./mainHeader/MainHeader";
 import Footer from "./Footer";
 import classes from "./Layout.module.css";
 
 const Layout = ({ children }) => {
-   const [showImageHomePage, setShowImageHomePage] = useState(true);
+   const [showImageHomePage, setShowImageHomePage] = useState(false);
+   const location = useLocation();
+
+   useEffect(() => {
+      if (location.pathname === "/home") {
+         setShowImageHomePage(true);
+      } else setShowImageHomePage(false);
+   }, [location.pathname]);
 
    const imageHomePage = (
       <div className={classes["container_initial-message"]}>
+         <div className={classes.image_home_message}></div>
+         <div className={classes.image_home_message}></div>
          <span>Find more than 300.0000 recipes around the world!</span>
       </div>
    );
@@ -15,7 +25,7 @@ const Layout = ({ children }) => {
    return (
       <Fragment>
          <MainHeader />
-         {imageHomePage}
+         {showImageHomePage && imageHomePage}
          <div
             className={`${classes["container_main-footer"]} ${
                showImageHomePage ? classes.showImageHome : ""
